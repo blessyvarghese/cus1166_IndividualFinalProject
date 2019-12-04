eff9dbd8089b950a2c28d75e9200bf985b669498
blessyvarghese <blessy.varghese16@stjohns.edu>
2019-12-04 16:42:21 -0500

    latest

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,21 @@
-
 # import flask_wtf
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, TextAreaField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, DateField, TimeField
 from wtforms.validators import ValidationError, DataRequired, Length
-
-
 
 
 class TaskForm(FlaskForm):
     task_desc = StringField('task_desc', validators=[DataRequired()])
-    task_status_completed = SelectField('Status', choices=[('todo','Todo'),('doing','Doing'),('done','Done')])
+    task_status_completed = SelectField('Status', choices=[('todo', 'Todo'), ('doing', 'Doing'), ('done', 'Done')])
     submit = SubmitField('submit')
+
+
+class AppointmentForm(FlaskForm):
+    Appointment_title = StringField('Appointment Title: ', validators=[DataRequired()])
+    Appointment_date = DateField('Appointment Date: ', validators=[DataRequired()])
+    Appointment_time = TimeField('Start Time: ', validators=[DataRequired()])
+    Appointment_duration = TimeField('Duration: ', validators=[DataRequired()])
+    Appointment_location = StringField('Client Address: ', validators=[DataRequired()])
+    Appointment_customer = StringField('Client Name: ', validators=[DataRequired()])
+    Appointment_notes = StringField('Notes: ', validators=[DataRequired()])
+    submit = SubmitField('Add Appointment')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,26 +1,26 @@
-from flask import render_template,  redirect, url_for
+from flask import render_template, redirect, url_for
 from app.main import bp
 from app import db
-from app.main.forms import TaskForm
-from app.models import Task
+from app.main.forms import TaskForm, AppointmentForm
+from app.models import Task, Appointments
 
 
 # Main route of the applicaitons.
-@bp.route('/', methods=['GET','POST'])
+@bp.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("main/index.html")
 
 
 #
 #  Route for viewing and adding new tasks.
-@bp.route('/todolist', methods=['GET','POST'])
+@bp.route('/todolist', methods=['GET', 'POST'])
 def todolist():
     form = TaskForm()
 
     if form.validate_on_submit():
         # Get the data from the form, and add it to the database.
         new_task = Task()
-        new_task.task_desc =  form.task_desc.data
+        new_task.task_desc = form.task_desc.data
         new_task.task_status = form.task_status_completed.data
 
         db.session.add(new_task)
@@ -31,14 +31,13 @@ def todolist():
 
     todo_list = db.session.query(Task).all()
 
-    return render_template("main/todolist.html",todo_list = todo_list,form= form)
+    return render_template("main/todolist.html", todo_list=todo_list, form=form)
 
 
 #
 # Route for removing a task
-@bp.route('/todolist/remove/<int:task_id>', methods=['GET','POST'])
+@bp.route('/todolist/remove/<int:task_id>', methods=['GET', 'POST'])
 def remove_task(task_id):
-
     # Query database, remove items
     Task.query.filter(Task.task_id == task_id).delete()
     db.session.commit()
@@ -49,7 +48,7 @@ def remove_task(task_id):
 #
 # Route for editing a task
 
-@bp.route('/todolist/edit/<int:task_id>', methods=['GET','POST'])
+@bp.route('/todolist/edit/<int:task_id>', methods=['GET', 'POST'])
 def edit_task(task_id):
     form = TaskForm()
     print(form.validate_on_submit())
@@ -57,7 +56,7 @@ def edit_task(task_id):
         # Get the data from the form, and add it to the database.
 
         current_task = Task.query.filter_by(task_id=task_id).first_or_404()
-        current_task.task_desc =  form.task_desc.data
+        current_task.task_desc = form.task_desc.data
         current_task.task_status = form.task_status_completed.data
 
         db.session.add(current_task)
@@ -69,7 +68,29 @@ def edit_task(task_id):
     current_task = Task.query.filter_by(task_id=task_id).first_or_404()
 
     # update the form model in order to populate the html form.
-    form.task_desc.data =     current_task.task_desc
+    form.task_desc.data = current_task.task_desc
     form.task_status_completed.data = current_task.task_status
 
-    return render_template("main/todolist_edit_view.html",form=form, task_id = task_id)
+    return render_template("main/todolist_edit_view.html", form=form, task_id=task_id)
+
+
+@bp.route('/AddAppointment', methods=['GET', 'POST'])
+def AddAppointment():
+    form = AppointmentForm()
+    if form.validate_on_submit():
+        new_Appointment = Appointments()
+        new_Appointment.Appointment_title = form.Appointment_title.data
+        new_Appointment.Appointment_date = form.Appointment_date.data
+        new_Appointment.Appointment_time = form.Appointment_time.data
+        new_Appointment.Appointment_duration = form.Appointment_duration.data
+        new_Appointment.Appointment_location = form.Appointment_location.data
+        new_Appointment.Appointment_Customer = form.Appointment_customer.data
+        new_Appointment.Appointment_notes = form.Appointment_notes.data
+        print("WENT THROUGH")
+        db.session.add(new_Appointment)
+        db.session.commit()
+        return redirect(url_for('main.AddAppointment'))
+
+    Appointments_added = db.session.query(Appointments).all()
+
+    return render_template("AddAppointment.html", Appointments_added=Appointments_added, form=form)
